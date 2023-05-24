9b6f66c6cec802d032416d58a2f01228b9ac68f4
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-05-24 20:51:36 +0800

    Create Zhihu(VIP).js

---
+++ Rewrite/修正规则/Zhihu(VIP).js
@@ -0,0 +1,5 @@
+#知乎VIP
+#原链接：https://raw.githubusercontent.com/WeiRen0/Scripts/main/ZHVIP.js
+
+^http[s]?:\/\/api\.zhihu\.com\/(people\/self|unlimited\/go\/my_card|sku\/reversion_sku_ext).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/ZHVIP.js
+hostname=api.zhihu.com
