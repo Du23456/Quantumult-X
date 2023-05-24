5c668775f4d724386736a0b1441a4d4027c6f9c2
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-05-24 20:42:46 +0800

    Create Xi ma la ya(VIP).js

---
+++ Rewrite/修正规则/Xi ma la ya(VIP).js
@@ -0,0 +1,23 @@
+#喜马拉雅VIP
+
+
+#原链接线路4
+#
+#原链接线路3
+#https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMSSVIP3.js
+#原链接线路2
+https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP2.js
+#原链接线路1
+#https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP01.js
+
+
+#线路4
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP04.js
+#线路3
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP03.js
+#线路2
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP2.js
+#线路1
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP.js
+
+hostname=180.153.*.*,*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com
