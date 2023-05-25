82ac1ead20116bcddb3e7c44109a74d6870198fe
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-05-25 19:03:32 +0800

    Create WPS(VIP).js

---
+++ Rewrite/修正规则/WPS(VIP).js
@@ -0,0 +1,3 @@
+# ✅ WPS(国区)
+^https:\/\/(vas|account)\.wps\.cn\/(query\/api\/v\d\/list_purchase_info|api\/users\/\d+\/overview) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
+hostname=*.wps.*
