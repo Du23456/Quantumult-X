c082579ba511606e27a62b95e421b92f21ec091f
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-05-25 19:01:08 +0800

    Delete Keep(VIP).js

--- Rewrite/提取规则/Keep(VIP).js
+++
@@ -1,3 +0,0 @@
-# keep解锁会员    *.gotokeep.com
-^https://(.+).gotokeep.com(/athena/v5/people/my|/nuocha/plans) url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/js/keep.js
-hostname=*.gotokeep.com
