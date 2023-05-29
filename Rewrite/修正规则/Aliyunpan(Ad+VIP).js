823b38768fdb974c79f97278c0d9fc4de2b777ce
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-05-29 14:26:16 +0800

    更新 Aliyunpan(Ad+VIP).js

--- Rewrite/修正规则/Aliyunpan(Ad+VIP).js
+++ Rewrite/修正规则/Aliyunpan(Ad+VIP).js
@@ -1,5 +1,4 @@
 #更新时间:2023-5-25 21:35
-
 # 阿里云盘 净化/解锁SVIP
 ^https?:\/\/(api|member)\.aliyundrive\.com url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/alidrive.js
 hostname = *.aliyundrive.com
