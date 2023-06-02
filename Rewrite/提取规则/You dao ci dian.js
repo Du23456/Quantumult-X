08df23c3443aeb7a239d829b3b194b1547d8bff9
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-03 00:04:48 +0800

    更新 You dao ci dian.js

--- Rewrite/提取规则/You dao ci dian.js
+++ Rewrite/提取规则/You dao ci dian.js
@@ -1,6 +1,7 @@
 #更新时间:2023-5-10 23:00
-
 # > 有道词典
+
+#####6⃣️#####
 ^https?:\/\/oimage([a-z])([0-9])\.ydstatic\.com\/.+adpublish url reject
 ^https?:\/\/oimage\w\d\.ydstatic\.com\/image\?.+?=adpublish url reject-img
 ^https?:\/\/dsp-impr2\.youdao\.com\/adload url reject-200
@@ -9,4 +10,5 @@
 #####1⃣️#####
 # 网易有道词典+解锁VIP    
 ^https:\/\/dict\.youdao\.com\/vip\/user\/status url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/wyydcd.js
+
 hostname=oimage*.ydstatic.com, dsp-impr2.youdao.com,impservice.dictapp.youdao.com,oral.youdao.com,dict.youdao.com
