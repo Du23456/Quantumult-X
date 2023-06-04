d9fe12bdf2f5fb7f4e6a6f34ff65fceedaa46c33
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-04 16:07:09 +0800

    更新 Taobao BJ.js

--- Rewrite/修正规则/Taobao BJ.js
+++ Rewrite/修正规则/Taobao BJ.js
@@ -1,5 +1,6 @@
-^http://.+/amdc/mobileDispatch script-request-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
-^http://.+/amdc/mobileDispatch script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
-^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.data\.get script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+# 淘宝比价
+^http://.+/amdc/mobileDispatch url script-request-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+^http://.+/amdc/mobileDispatch url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+^https?://trade-acs\.m\.taobao\.com/gw/mtop\.taobao\.detail\.data\.get url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
 
 hostname=trade-acs.m.taobao.com
