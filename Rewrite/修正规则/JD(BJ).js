61f004e66655f5f8a357d29b1cf0f386e9cbb156
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-04 16:11:19 +0800

    Create JD(BJ).js

---
+++ Rewrite/修正规则/JD(BJ).js
@@ -0,0 +1,4 @@
+# 京东比价
+^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
+
+hostname = api.m.jd.com
