68c056d20a5369f0da0c9c96a3e9e0a23223f2e0
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-05 14:21:51 +0800

    Delete WPS(VIP).js

--- Rewrite/修正规则/WPS(VIP).js
+++
@@ -1,7 +0,0 @@
-#更新时间:2023-5-25 21:35
-#WPS解锁会员
-#原链接：https://raw.githubusercontent.com/ddgksf2013/dev/master/ForOwnUse.conf
-
-# ✅ WPS(国区)
-^https:\/\/(vas|account)\.wps\.cn\/(query\/api\/v\d\/list_purchase_info|api\/users\/\d+\/overview) url script-response-body https://raw.githubusercontent.com/510004015/Quantumult_X/Remote/Premium/WPSOffice.js
-hostname=*.wps.*
