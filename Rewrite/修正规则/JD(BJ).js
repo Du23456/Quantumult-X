2b2c3999792f95124d91be8596d6ae88a2198b8f
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-11 16:33:57 +0800

    Delete JD(BJ).js

--- Rewrite/修正规则/JD(BJ).js
+++
@@ -1,6 +0,0 @@
-# 京东比价
-# 原项目链接:https://service.2ti.st/QuanX/Rewrite/jd_tb_price.conf
-
-^https?://api\.m\.jd\.com/client\.action\?functionId=(wareBusiness|serverConfig|basicConfig) url script-response-body https://service.2ti.st/QuanX/Script/jd_tb_price/main.js
-
-hostname = api.m.jd.com
