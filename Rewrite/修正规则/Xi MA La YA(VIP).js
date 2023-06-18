2c488b5ad80b4baedf05f43e94444f7edb5cabc1
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-19 00:19:03 +0800

    Create Xi MA La YA(VIP).js

---
+++ Rewrite/修正规则/Xi MA La YA(VIP).js
@@ -0,0 +1,7 @@
+#更新时间:2023-6-17 20:15
+#喜马拉雅VIP
+#原项目链接:https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
+
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
+
+hostname=*xima*,*xmcdn*,*.ximalaya.com
