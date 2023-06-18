06ca7112be576bf9dc709da7650e6ea809990b9e
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-19 00:15:06 +0800

    更新 Xi ma la ya(VIP).js

--- Rewrite/修正规则/Xi ma la ya(VIP).js
+++ Rewrite/修正规则/Xi ma la ya(VIP).js
@@ -4,4 +4,4 @@
 
 ^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
 
-hostname=180.153.*.*,*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com
+hostname=*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com
