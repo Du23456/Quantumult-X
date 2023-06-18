47ebed8a3d1e477bfefe32e022fa8896e5f06070
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-06-18 20:14:22 +0800

    更新 Xi ma la ya(VIP).js

--- Rewrite/修正规则/Xi ma la ya(VIP).js
+++ Rewrite/修正规则/Xi ma la ya(VIP).js
@@ -1,30 +1,7 @@
-#更新时间:2023-6-7 22:45
+#更新时间:2023-6-17 20:15
 #喜马拉雅VIP
+#原项目链接:https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
 
-#原链接新线路1
-#https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPCS1.js
-#原链接新线路2
-#https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPCS2.js
-#原链接线路4
-#https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP04.js
-#原链接线路3
-#https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMSSVIP3.js
-#原链接线路2
-https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP2.js
-#原链接线路1
-#https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP01.js
-
-#新线路1
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPCS1.js
-#新线路2
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPCS2.js
-#线路4
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP04.js
-#线路3
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMVIP03.js
-#线路2
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP2.js
-#线路1
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/NewFree/main/XMSVIP.js
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
 
 hostname=180.153.*.*,*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com
