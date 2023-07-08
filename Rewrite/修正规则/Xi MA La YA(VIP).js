a1759eaa2141b54a53a86f7d0b6e936fa3d50f7b
Du23456 <127875782+Du23456@users.noreply.github.com>
2023-07-08 12:00:53 +0800

    更新 Xi MA La YA(VIP).js

--- Rewrite/修正规则/Xi MA La YA(VIP).js
+++ Rewrite/修正规则/Xi MA La YA(VIP).js
@@ -1,7 +1,11 @@
-#更新时间:2023-6-17 20:15
+#更新时间:2023-7-8 12:00
 #喜马拉雅VIP
-#原项目链接:https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
+#原项目链接1:https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY1.js
+#原项目链接3:https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY3.js
 
-^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY.js
+#线路1
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY1.js
+#线路3
+^http[s]?:\/\/((.+ximalaya.+)|(.+xmcdn.+)).*\/(playpage\/tabs\/v2|v1\/album\/track\/ts|v1\/album\/price|v2\/homePage|business-vip-presale-mobile-web\/page\/ts|vip\/v1\/recommand|track\/v3\/baseInfo|v1\/module\/data\/dynamic|basicInfo\/dynamic\/ts).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/XMVIPTY3.js
 
 hostname=*xima*,*xmcdn*,*.ximalaya.com
