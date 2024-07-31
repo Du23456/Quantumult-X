3a68b43cf6e2dc0d5fb456c3a987cc5cb3db32aa
Du23456 <127875782+Du23456@users.noreply.github.com>
2024-07-31 21:39:41 +0800

    Delete Rewrite/修正规则 directory

--- Rewrite/修正规则/Zhihu(VIP).js
+++
@@ -1,6 +0,0 @@
-#更新时间:2023-5-25 21:35
-#知乎VIP
-#原链接：https://raw.githubusercontent.com/WeiRen0/Scripts/main/ZHVIP.js
-
-^http[s]?:\/\/api\.zhihu\.com\/(people\/self|unlimited\/go\/my_card|sku\/reversion_sku_ext).*$ url script-response-body https://raw.githubusercontent.com/WeiRen0/Scripts/main/ZHVIP.js
-hostname=api.zhihu.com
